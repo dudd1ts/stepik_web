72abce6f7df32b37f43b50d460e6ba8849d9b7be
dudd1ts <kserg90@gmail.com>
2016-11-08 14:36:32 +0300

    Step 1.8.12

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
