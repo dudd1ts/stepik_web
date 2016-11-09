134e5bfc57f3092e027b6a6e6bbb9e2ea7875359
dudd1ts <kserg90@gmail.com>
2016-11-09 12:37:46 +0300

    Step 1.9.11

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ fi
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo /etc/init.d/gunicorn restart
